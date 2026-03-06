904b78023a938fc903fb79001fd22cc3b7366aea
priyanshi1933 <pd.opash@gmail.com>
2026-03-06 18:06:36 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   const socket = useMemo(() => io("http://localhost:3000"), []);
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
+   const [roomName, setRoomName] = useState("");
   const [socketID, setSocketID] = useState("");
   const [messages,setMessages]=useState([]);
   console.log(messages);
@@ -17,6 +18,11 @@ const App = () => {
     socket.emit("message", {message,room});
     setMessage("");
   };
+  const joinRoomHandler=(e)=>{
+    e.preventDefault();
+    socket.emit("join-room",roomName);
+    setRoomName("");
+  }
   useEffect(() => {
 
     socket.on("connect", () => {
@@ -42,7 +48,19 @@ const App = () => {
         {socketID}
       </Typography>
 
-      
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
+         <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+         <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+      </form>
 
       <form onSubmit={handleSubmit}>
         <TextField
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import cors from 'cors';
+import cors from "cors";
 
 const port = 3000;
 
@@ -10,17 +10,19 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174",
-    methods:["GET","POST"],
-    credentials:true,
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
-app.use(cors({
-    origin: "http://localhost:5174",
-    methods:["GET","POST"],
-    credentials:true,
-  }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -28,14 +30,18 @@ app.get("/", (req, res) => {
 
 //create circuit
 io.on("connection", (socket) => {
-  console.log("User Connected",socket.id);
-  socket.on("message",({room,message})=>{
-    console.log({room,message});
-    socket.to(room).emit("receive-message",message);
-  })
-  socket.on("disconnect",()=>{
-    console.log("User Disconnected",socket.id);
-  })
+  console.log("User Connected", socket.id);
+  socket.on("message", ({ room, message }) => {
+    console.log({ room, message });
+    socket.to(room).emit("receive-message", message);
+  });
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
 server.listen(port, () => {
